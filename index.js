8fbcf0d66fd0a17415fbeafa632bff75ccf7a583
giridharan s <160309516+gdharan361@users.noreply.github.com>
2024-08-22 20:17:48 +0530

    save

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { mongoBD } from "./Database/config.js";
+import { router } from "./Routers/sdumenRouter.js";
+
+dotenv.config();
+const app = express();
+const PORT = 4000;
+app.use(cors());
+app.use(express.json());
+mongoBD();
+
+app.use("/api", router);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome back");
+});
+
+app.listen(process.env.PORT, (req, res) => {
+  console.log("App is running on");
+});
